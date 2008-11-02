04f15ccf7bf45d84620369b6411ff00d3a358988
Johan Kinnander <johan.kinnander@cybercom.com>
2008-11-02 17:18:52 +0000

    Test commit. Created LineFollowerMoudle folder and header file.

---
+++ src/LineFollowerModule/LineFollowerModule.h
@@ -0,0 +1,23 @@
+/*
+ * LineFollowerModule.h
+ *
+ *  Created on: Nov 2, 2008
+ *      Author: johan
+ *
+ *  Description:
+ *  TBD
+ */
+
+#ifndef LINEFOLLOWERMODULE_H_
+#define LINEFOLLOWERMODULE_H_
+
+class LineFollower :: public CompetitionModule
+{
+public:
+
+
+private:
+
+}
+
+#endif /* LINEFOLLOWERMODULE_H_ */
