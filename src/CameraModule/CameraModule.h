5dc43c0720529c3e89941505ccf365885a26bf81
Johan Kinnander <johan.kinnander@cybercom.com>
2008-11-22 16:57:14 +0000

    Updated CameraModule_Descr.txt. Introduced some types and member function 'horizontalSweep()' in CameraModule.h. Function not defined; does not build.

--- src/CameraModule/CameraModule.h
+++ src/CameraModule/CameraModule.h
@@ -10,11 +10,33 @@
 
 using namespace std;
 
+typedef unsigned int PictureCoordinate_t;
+
+typedef struct WhiteSegmentDescriptor
+{
+  PictureCoordinate_t Leftmost;
+  PictureCoordinate_t Rightmost;
+  PictureCoordinate_t Center;
+  PictureCoordinate_t Width;
+} WhiteSegmentDescriptor_t;
+
+#define WHITE_SEGMENT_ARRAY_LENGTH 10
+
+typedef struct WhiteSegmentArray
+{
+  WhiteSegmentDescriptor_t[WHITE_SEGMENT_ARRAY_LENGTH];  // TODO: Syntax error?!
+  unsigned int NumberOfWhiteSegments;
+} WhiteSegmentArray_t;
+
 class CameraModule : public IEventGenerator, ICommandServer {
 private:
 	list<IEventListener*> positionErrorListeners;
 	list<IEventListener*> intersectionFoundListeners;
 	list<IEventListener*> numberFoundListeners;
+
+	// TODO: Write function description here?
+	WhiteSegmentArray_t horizontalSweep( PictureCoordinate_t startCoordinate, PictureCoordinate_t sweepWidth );
+
 public:
 	typedef enum {
 		CMD_SNAPSHOT,
